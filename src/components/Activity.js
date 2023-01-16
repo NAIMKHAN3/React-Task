d697d85ae69f8dad167a4bb360b6b728747e9ed7
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-01-16 17:05:40 +0600

    Activity Added

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,13 +3,32 @@ import React from 'react';
 const Activity = () => {
     return (
         <div className='p-3 m-3'>
-            <h1>Recent Activity</h1>
-            <div>
-                <div>
-
+            <h1 className='font-bold mb-5'>Recent Activity</h1>
+            <div className='flex'>
+                <div className='rounded-md p-1 m-2 bg-base-200 w-[70px] text-center'>
+                    <h1 className='font-bold'>12</h1>
+                    <p className='font-bold'>Sep</p>
                 </div>
                 <div>
-
+                    <h1>Responded to need "volunteer Activities"</h1>
+                </div>
+            </div>
+            <div className='flex'>
+                <div className='rounded-md p-1 m-2 bg-base-200 w-[70px] text-center'>
+                    <h1 className='font-bold'>11</h1>
+                    <p className='font-bold'>Sep</p>
+                </div>
+                <div>
+                    <h1>Everyone realizes would be desirable... <a className='text-primary' href="">Read more</a></h1>
+                </div>
+            </div>
+            <div className='flex'>
+                <div className='rounded-md p-1 m-2 bg-base-200 w-[70px] text-center'>
+                    <h1 className='font-bold'>10</h1>
+                    <p className='font-bold'>Sep</p>
+                </div>
+                <div>
+                    <h1>Joined the group "Boardsmanship Forum"</h1>
                 </div>
             </div>
         </div>
